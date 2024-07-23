1ac48db9680ec817dddcca0028f84eb975c48571
Kevin Alberca <kalberca@iteprevengo.com>
2024-07-22 23:17:33 -0500

    configurando swagger

--- source/swagger/index.ts
+++ source/swagger/index.ts
@@ -2,25 +2,31 @@ import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import path from 'path';
 
+
 // Configuración de Swagger
 const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
+    definition: {
+        openapi: '3.0.1',
         info: {
-            title: 'API Documentation',
-            version: '1.0.0',
-            description: 'API Information',
+            title: 'User Management API',
+            description: 'API para obtener la información de un usuario por ID.',
             contact: {
-                name: 'Developer',
+                name: 'kevo',
             },
-            servers: [
-                {
-                    url: 'http://localhost:3000',
-                },
-            ],
+            version: '1.0.0',
         },
+        // servers: [
+        //     {
+        //         url: '/v1',
+        //     },
+        // ]
+
     },
-    apis: [path.join(__dirname, './swaggerDoc.ts')],
+    apis: [path.join(__dirname, 'swaggerDoc.ts')],
+    //apis: [path.join(__dirname, 'swagger.yaml')],
+    // apis: [path.join(__dirname, 'swagger.json')],
+
+
 
 };
 
