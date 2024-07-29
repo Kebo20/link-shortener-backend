e12aa0c2a921dade0e9f2366e28d5201d12a7198
Kevin Alberca <kalberca@iteprevengo.com>
2024-07-29 13:12:10 -0500

    correción cors

--- source/infrastructure/middlewares/cors.ts
+++ source/infrastructure/middlewares/cors.ts
@@ -1,9 +1,10 @@
 import cors from 'cors'
+import { HttpError } from '../utils/handleError';
 
 const ACCEPTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://localhost:1234',
-    'https://movies.com',
+    // 'http://localhost:8080',
+    // 'http://localhost:1234',
+    // 'https://movies.com',
     'https://midu.dev'
 ]
 
@@ -11,14 +12,26 @@ const ACCEPTED_ORIGINS = [
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
     origin: (origin, callback) => {
+        console.log(origin)
         if (acceptedOrigins.includes(`${origin}`)) {
             return callback(null, true)
         }
 
         if (!origin) {
             return callback(null, true)
+
+            // return callback(new HttpError({
+            //     code: 'FORBIDDEN',
+            //     message: 'Not allowed by CORS',
+            // }))
         }
 
-        return callback(new Error('Not allowed by CORS'))
+        // return callback(new Error('Not allowed by CORS'))
+        return callback(new HttpError({
+            code: 'FORBIDDEN',
+            message: 'Not allowed by CORS',
+        }))
+
+
     }
 })
