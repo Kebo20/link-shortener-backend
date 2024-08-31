29c53f7167a9fe18ef7f2f6b66869588f6c1f62d
Kevin Alberca <kalberca@iteprevengo.com>
2024-08-31 18:18:03 -0500

    validacion de link

--- source/infrastructure/middlewares/link.ts
+++ source/infrastructure/middlewares/link.ts
@@ -12,8 +12,8 @@ export const validatorRegisterLink = async (req: Request, res: Response, next: N
             }),
             description: z.string().min(0).max(250).optional(),
             password: z.string().min(4).max(8).optional(),
-            shortUrl: z.string().min(6).max(250).optional().refine((url) => url === undefined || /^[a-zA-Z0-9]+$/.test(url), {
-                message: "La URL corta solo debe contener caracteres alfanuméricos",
+            shortUrl: z.string().min(6).max(250).optional().refine((url) => url === undefined || /^[a-zA-Z0-9-_]+$/.test(url), {
+                message: "La URL corta solo debe contener caracteres alfanuméricos o guiones",
             })
 
 
